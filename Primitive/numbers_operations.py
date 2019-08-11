5e53ece9f8f608a7f0928ead857bc9ec7c879fc9
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-11 18:53:29 +0530

    Number Operations

---
+++ Primitive/numbers_operations.py
@@ -0,0 +1,21 @@
+# Python Operation in Numbers
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+# Two type of division single / 'slash' used for floating poing and '//' used for integer value
+print(10 / 3)
+print(10 // 3)
+
+# remender
+print(10 % 3)
+
+# Exponent ex 10 to the power of 3
+print(10 ** 3)
+
+
+x = 1
+y = 1.1
+
+# Python support comptex number Ex : a + bi
+
+z = 1 + 2j
