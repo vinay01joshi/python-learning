99dc5949b49972e0305b3822d1bc6a4d294a5826
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-11 18:38:11 +0530

    String formatted Exaple

---
+++ Primitive/formatted_string.py
@@ -0,0 +1,8 @@
+first = "Vinay"
+last = "Joshi"
+full = first + " " + last
+full_formated = f"{first} {last}"
+full_expresstion = f"{len(first)} {last}"
+print(full)
+print(full_formated)
+print(full_expresstion)
