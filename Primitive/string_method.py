3245a903020b7ec99a7bf07369c8d77c0b9cee02
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-11 18:47:15 +0530

    String Methods Examples

---
+++ Primitive/string_method.py
@@ -0,0 +1,10 @@
+cource = " python programming    "
+print(cource.upper())
+print(cource.lower())
+print(cource.title())
+print(cource.strip())
+print(cource.find("pro"))
+print(cource.replace("p", "j"))
+# Expression in String
+print("pro" in cource)
+print("swift" not in cource)
