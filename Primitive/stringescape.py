6e6eaef0e6a6fbc5eeffc407a50b21f717eda1a4
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-11 18:33:51 +0530

    String escape characters

---
+++ Primitive/stringescape.py
@@ -0,0 +1,12 @@
+# Python Escape Characters
+# \"
+# \'
+# \\
+# \n
+
+course = 'Python "Programming'
+cource_escape = "Python \"Programming"
+cource_newline = "Python \nProgramming"
+print(cource_escape)
+print(course)
+print(cource_newline)
