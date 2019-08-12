e932d3911d20571a4a9594d3145c90e184a7c991
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-12 12:04:57 +0530

    For Else Statement

--- ControlFlow/for_loop.py
+++ ControlFlow/for_loop.py
@@ -3,3 +3,13 @@ for number in range(1, 4):
 
 for number in range(1, 10, 2):
     print("Attempt", number, number * ".")
+
+    # Successflly sned a meesage
+successful = True
+for number in range(3):
+    print("Attempt")
+    if successful:
+        print("Successful Snet a Message")
+        break
+else:
+    print("Attempated 3 times and its failed")
