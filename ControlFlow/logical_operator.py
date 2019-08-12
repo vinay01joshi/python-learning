01d05ae7acef9f41380e725b835dbbf29b16d5cd
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-12 11:49:16 +0530

    Logical Operator

---
+++ ControlFlow/logical_operator.py
@@ -0,0 +1,9 @@
+high_income = True
+good_credit = False
+student = False
+# and , or , not
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not Eligitble")
