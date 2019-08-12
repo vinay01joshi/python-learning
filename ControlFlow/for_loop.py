7a83d1aa43675390b4bda112017f1935a8f0e785
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-12 12:08:47 +0530

    Nested Loops in Python

--- ControlFlow/for_loop.py
+++ ControlFlow/for_loop.py
@@ -13,3 +13,9 @@ for number in range(3):
         break
 else:
     print("Attempated 3 times and its failed")
+
+
+# Nested Loop
+for x in range(5):
+    for y in range(3):
+        print(f"({x},{y})")
