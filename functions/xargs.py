852af957731b381cb7bec3638507765dc29014a3
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-13 12:04:59 +0530

    xargs in Pyton

---
+++ functions/xargs.py
@@ -0,0 +1,12 @@
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print(multiply(1, 2, 5, 6, 7))
+
+# [] squre bracker called list
+# () praenthesis calle tuples
+# difference between list and tuples are we can not modify the collection
