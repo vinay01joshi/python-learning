40c032e82c6f31cdcede7eeffc831968f6f093b1
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-14 11:50:09 +0530

    Debuging in python

---
+++ functions/debugging_function.py
@@ -0,0 +1,13 @@
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+
+print(multiply(1, 2, 3, 4, 5))
+
+# F5 TO RUN THE PROGRAM
+# F10 TO STEP WISE STEP
+# F11 TO GO INSIDE THE FUNCTION
+# SHIFT F11 TO TERMINATE THE DEBUGGER
