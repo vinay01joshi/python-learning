b50e18da2addac7dfa6c8f7766aa4f7c12480669
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-14 12:14:23 +0530

    Accessling List

--- DataStructure/list.py
+++ DataStructure/list.py
@@ -11,3 +11,19 @@ combine = zeros + letters
 numbers = list(range(20))
 chars = list("Vinay Joshi")
 print(len(chars))
+
+# Accessing Items in list
+
+letters[0] = "A"
+print(letters[0])
+print(letters[-1])
+print(letters[0:3])
+# copy of list
+print(letters[:])
+# every iterativ item from list
+print(letters[::2])
+print(letters)
+
+number = list(range(20))
+print(number[::2])
+print(number[::-1])
