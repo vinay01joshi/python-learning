fe508c8d7ae40fe43ba42e168db09c9d30c4eb80
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-14 12:09:14 +0530

    Built In Data Structure | List in python

---
+++ DataStructure/list.py
@@ -0,0 +1,13 @@
+letters = ["a", "b", "c", "d"]
+matrix = [[0, 1], [0, 2], [0, 3]]
+
+# Repeat the item in list
+zeros = [0] * 5
+
+# Combine the list
+combine = zeros + letters
+
+# numbers in list
+numbers = list(range(20))
+chars = list("Vinay Joshi")
+print(len(chars))
