f185196ef9eb3c42a37d63d23ed2ee5ec025e3d6
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-15 13:31:14 +0530

    Hanelling Different Exceptoin

--- Exceptions/exceptions_handling.py
+++ Exceptions/exceptions_handling.py
@@ -1,9 +1,7 @@
 try:
     age = int(input("Age: "))
-except ValueError as ex:
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError) as ex:
     print("Please enter a valid age")
-    print(ex)
-    print(type(ex))
 else:
     print("No exceptions were thorwn")
-print("Program continues")
