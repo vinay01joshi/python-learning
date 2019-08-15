e50e925f2c8f94589f707f7eccb0daaae93f1392
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-15 14:05:05 +0530

    Raising Exception with Performance Impact

--- Exceptions/raising_exception.py
+++ Exceptions/raising_exception.py
@@ -1,3 +1,6 @@
+from timeit import timeit
+
+code1 = """
 def calculate_xfactor(age):
     if age <= 0:
         raise ValueError("Age can not be zero or Less")
@@ -7,4 +10,19 @@ def calculate_xfactor(age):
 try:
     calculate_xfactor(-1)
 except ValueError as error:
-    print(error)
+    pass
+"""
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10/age
+
+
+xfactor = calculate_xfactor(-1)
+if xfactor == None:
+    pass
+"""
+
+print("FirstCode :", timeit(code1, number=10000))
+print("SecondCode :", timeit(code2, number=10000))
