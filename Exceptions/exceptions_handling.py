bfbc53eba94cac57d54317fb979138317d9f9e0d
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-15 13:37:27 +0530

    with statement in python.

--- Exceptions/exceptions_handling.py
+++ Exceptions/exceptions_handling.py
@@ -1,10 +1,10 @@
 try:
-    file = open("app.py")
+    #with operator is just like using keyword in c#
+    with open("app.py") as file:
+        print("File Opend")
     age = int(input("Age: "))
     xfactor = 10 / age
 except (ValueError, ZeroDivisionError) as ex:
     print("Please enter a valid age")
 else:
     print("No exceptions were thorwn")
-finally:
-    file.close()
