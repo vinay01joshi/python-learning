3944244b9e3e600075ce427cc55434f19958c752
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-15 14:30:13 +0530

    class vs instance

--- classes/point.py
+++ classes/point.py
@@ -1,4 +1,7 @@
 class Point:
+    # class level attribute shared with all instance of claases
+    default_color = "red"
+
     # self is the refrence of current point class object
     def __init__(self, x, y):
         self.x = x
@@ -9,4 +12,11 @@ class Point:
 
 
 point = Point(1, 2)
+point.z = 11
+Point.default_color = "Yellow"
 point.draw()
+print(point.default_color)
+print(Point.default_color)
+
+point2 = Point(5, 6)
+point2.draw()
