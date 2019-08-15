ac9524dda70a4991f9cfc6b346143c3dc13a3837
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-15 16:27:25 +0530

    Inheritance in pyhton

---
+++ classes/ObjectOriented/inheriteance.py
@@ -0,0 +1,25 @@
+class Animal:
+
+    def __init__(self):
+        self.age = 1
+
+    def eat(self):
+        print("eat")
+
+# Animal : Parent or Base class
+# Mamel  : Child or  Sub  Class
+
+
+class Mamel(Animal):
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+m = Mamel()
+m.eat()
+print(m.age)
