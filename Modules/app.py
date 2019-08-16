283505a9e6bd45f70ead19d7492b5759a3a1ed69
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-16 09:33:14 +0530

    dir fuction

--- Modules/app.py
+++ Modules/app.py
@@ -1,3 +1,5 @@
-from ecommerce.shopping.sales import calc_shipping, calc_tax
+from ecommerce.shopping import sales
 
-calc_shipping()
+print(dir(sales))
+print(sales.__file__)
+print(sales.__name__)
