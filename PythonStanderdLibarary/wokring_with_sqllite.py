d5860d26f793a9abadc951ead3247c4c7d6c10ca
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-20 11:58:30 +0530

    working with sqllite

---
+++ PythonStanderdLibarary/wokring_with_sqllite.py
@@ -0,0 +1,22 @@
+import sqlite3
+import json
+from pathlib import Path
+
+# Writing sql lite
+# movies = json.loads(Path("movies.json").read_text())
+
+# with sqlite3.connect("db.sqlite3") as conn:
+#     command = "INSERT INTO Movies VALUES(?, ?, ?)"
+#     for movie in movies:
+#         conn.execute(command, tuple(movie.values()))
+#     conn.commit()
+#
+
+#  Reading sqlliste
+with sqlite3.connect("db.sqlite3") as conn:
+    command = "SELECT * FROM Movies"
+    cursor = conn.execute(command)
+    # for row in cursor:
+    #     print(row)
+    movies = cursor.fetchall()
+    print(movies)
