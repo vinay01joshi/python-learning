2e3ba82269d10f8aaf8eefe6ca252f3f89429082
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-22 11:41:02 +0530

    time delta

---
+++ PythonStanderdLibarary/wokring_with_time_delta.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+
+dt1 = datetime(2019, 1, 1) + timedelta(days=1, seconds=1000)
+print(dt1)
+dt2 = datetime.now()
+
+duration = dt2 - dt1
+print(duration)
+print("days", duration.days)
+print("seconds", duration.seconds)
+print("total_seconds", duration.total_seconds())
