e53a44f9714c2ee9e7e811fbb5d2f45c8052346d
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-28 12:20:36 +0530

    Documenting the methods and classes

--- vinpdf/vinpdf/pdf2text.py
+++ vinpdf/vinpdf/pdf2text.py
@@ -1,3 +1,16 @@
-def convert():
-    print("pdf2text")
-    
+""" This module provide functions to convert text from pdf document."""
+
+
+class Converter:
+    """A simple converter for converting pdf to text"""
+
+    def convert(self, path):
+        """Conver the given pdf to text.
+
+        Parameters:
+        path (str): The path to a pdf file.
+
+        Returns:
+        str: The content of the pdf file as text
+        """
+        print("pdf2text")
