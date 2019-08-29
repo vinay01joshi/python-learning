2aa142ce534113c924dd63dda32b6792bd77f60f
Vinay Joshi <vinay01joshi@gmail.com>
2019-08-29 12:09:52 +0530

    Business Searching

---
+++ pyyelp/app.py
@@ -0,0 +1,19 @@
+import requests
+url = "https://api.yelp.com/v3/businesses/search"
+api_key = ""
+headers = {
+    "Authorization": "Bearer " + api_key
+}
+params = {
+    "term": "Barber",
+    "location": "NYC"
+}
+response = requests.get(url, headers=headers, params=params)
+businesses = response.json()["businesses"]
+
+names = [business["name"]
+         for business in businesses if business["rating"] > 4.5]
+print(names)
+
+# for business in businesses:
+#     print(business["name"])
